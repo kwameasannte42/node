9b17cfa9f3cad452410059d60a065442d9acfd14
gigyen <coolllyod@gmail.com>
2023-02-28 07:47:47 -0500

    first push

---
+++ people.js
@@ -0,0 +1,9 @@
+const people = ['Arsenal', 'Manchester', 'Liverpool', 'Chealsea'];
+const age = [20, 45, 56, 49]
+
+// console.log(people);
+
+module.exports = {
+  people,
+  age
+};
---
+++ test.js
@@ -0,0 +1,11 @@
+// const name = "gideon";
+
+// console.log(name);
+
+const greet = (name) => {
+
+  console.log(`hello, $(name)`);
+}
+
+greet('nes');
+greet('pikachu')
